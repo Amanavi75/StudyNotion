c3c45cf652f836fa385e131c86bac6db2788c738
amanavi75 <amanavi75@gmail.com>
2024-02-25 00:06:04 +0530

    written utility function for sending otp through the email

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
     email:{
@@ -16,6 +17,25 @@ const OTPSchema = new mongoose.Schema({
 
     }
 
+})
+
+//*function for sending emails
+
+async function sendVerificationEmail(email,otp){
+    try{
+
+        const mailResponse = await mailSender(email,"verification Email from study notion",otp);
+        console.log("email sent successfully",mailResponse)
+    }
+    catch(error){
+        console.log("error while sending emails in verification process",error);
+        throw error;
+    }
+}
+
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp)
+    next();
 })
 
 module.exports= mongoose.model("OTP",OTPSchema)
