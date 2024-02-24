dd95113641bed04d0afd12cdd4a91c2d42489314
amanavi75 <amanavi75@gmail.com>
2024-02-24 14:43:05 +0530

    done with all the models

---
+++ server/models/Tags.js
@@ -0,0 +1,20 @@
+const mongoose = reuire("mongoose");
+
+const tagsSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+    },
+    course:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Course",
+    },
+
+
+})
+
+module.exports= mongoose.model("Tags",tagsSchema);
