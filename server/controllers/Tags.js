d626c8ef56ff197bb53d6f69fe7a6e3289cd170d
amanavi75 <amanavi75@gmail.com>
2024-03-06 22:53:11 +0530

    done with show all tags controller

--- server/controllers/Tags.js
+++ server/controllers/Tags.js
@@ -1,6 +1,6 @@
 const Tag = require("../models/Tags");
 
-//
+//create tag 
 exports.createTag = async(req,res)=>{
     try{
 
@@ -34,5 +34,25 @@ exports.createTag = async(req,res)=>{
             success:false ,
             message:"Unable to create tag "
         })
+    }
+}
+
+//getAll tags 
+exports.showAllTags = async(req,res)=>{
+    try{
+        const allTags = await Tag.find({},{name:true, description:true})
+        res.status(200).json({
+            success:true ,
+            message:"all tags generated successfully  ",
+            allTags,
+        })
+
+
+    }catch(error){
+        res.status(500).json({
+            success:false ,
+            message:"Unable to get list of all the  tag "
+        })
+
     }
 }
