739db204fe982a09ee9039142059145cb640e4a9
amanavi75 <amanavi75@gmail.com>
2024-03-08 22:42:49 +0530

    stucked in writting business logic for the courses controller

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -1,3 +1,15 @@
 const Course = require("../models/Course");
 const Tag = require("../models/Tags");
 const User = require("../models/User");
+const {uploadImageToCloudinary} = require("../utils/imageUploader")
+
+
+// createCourse handler function 
+exports.createCourse = async(res,res)=>{
+    try{
+        //
+
+    }catch(error){
+
+    }
+}
