aa7a1732469b7317f20f0a28be186c59d2ef5426
amanavi75 <amanavi75@gmail.com>
2024-03-18 20:15:08 +0530

    started with the payment gateway integration

--- server/controllers/Payment.js
+++ server/controllers/Payment.js
@@ -8,3 +8,13 @@ const mailSender = require("../utils/mailSender")
 
 const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail");
 
+
+//capture the payment and initiate the razorpay order
+exports.capturePayment = async(req,res)=>{
+    //get courseId and userId
+    //validation
+    //valid courseId
+    //valid CourseDetail
+    //user already pay for the same course
+    //return response
+}
