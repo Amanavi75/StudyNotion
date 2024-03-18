fded2a7d6b53c4eeeefaca5329125e4fef3eeb14
amanavi75 <amanavi75@gmail.com>
2024-03-18 20:12:05 +0530

    added templates for the course enrollment mail and email verification mail

--- server/models/Course.js
+++ server/models/Course.js
@@ -34,6 +34,10 @@ const courseSchema = new mongoose.Schema({
     type:String,
    },
    tag:{
+    type: [String],
+    required: true,
+   },
+   category:{
     type:mongoose.Schema.Types.ObjectId,
     ref:"Tag"
    },
