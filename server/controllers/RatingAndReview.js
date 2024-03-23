03b40158904d68ca40fff5862d4f062b3f4b8f15
amanavi75 <amanavi75@gmail.com>
2024-03-23 18:05:16 +0530

    done with all the controller of rating and review

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -66,6 +66,8 @@ exports.createRating= async(req,res)=>{
             },
             {new:true},
         )
+
+        console.log(updatedCourse);
         //return res
 
         return res.status(200).json({
@@ -83,10 +85,78 @@ exports.createRating= async(req,res)=>{
     }
      
 }
-
-
 //average rating  and review
+exports.getAverageRating  = async(req,res)=>{
+
+    try{
+
+        //get coursedId
+        const{courseId} = req.body.courseId;
+        //calculate average rating 
+        const result = await RatingAndReview.aggregate([
+            {
+                $match:{
+                    course: new mongoose.Types.ObjectId(courseId),
+                },
+            },
+            {
+                $group:{
+                    _id:null,
+                    averageRating:{$avg:"$rating"},
+                }
+            }
+        ])
+        //return rating 
+        if(result.length>0){ //  it means we have some rating
+            return res.status(200).json({
+                success:true,
+                averageRating:result[0].averageRating,
+            }) 
+        }
+
+        //if not rating review exist 
+        return res.status(200).json({
+            success:true,
+            message:"average rating is zero",
+            averageRating:0
+        })
+
+    }catch(error){
+
+        return res.status(500).json({
+            success:false,
+            message:"not able to find rating",
+            error:error.message,
+        })
+    }
+} 
+//getAll rating and reviews
+exports.getAllRating = async(req,req)=>{
+    try {
+        const allReviews = await RatingAndReview.find({})
+        .sort({rating:"desc"})
+        .populate({
+            path:"user",
+            select:"firstName lastName email image",
+        })
+        .populate({
+            path:"course",
+            select:"courseName",
+
+        })
+        .exec();
 
 
-
-//getAll rating and review 
+    return res.status(200).json({
+        success:true,
+        message:"all review fetched successfully",
+        data:allReviews
+    })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"not able to find rating or reciew",
+            error:error.message,
+        })
+    }  
+}
