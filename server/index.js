74e1ae0c0425846aed774d283dadaa25eb790e32
amanavi75 <amanavi75@gmail.com>
2024-03-29 15:13:42 +0530

    added cloudinary configuaration

--- server/index.js
+++ server/index.js
@@ -6,4 +6,10 @@ const app = express();
 const userRoutes = require("./routes/User")
 const courseRoutes = require("./routes/Course")
 const paymentRoutes = require("./routes/Payment")
-const profileRoutes = require("./routes/Profile")
+const profileRoutes = require("./routes/Profile");
+
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const {cloudinaryConnect} = require("./config/cloudinary")
