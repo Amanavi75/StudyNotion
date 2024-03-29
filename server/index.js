9e10894a6298bee38ae24b04825b887267fed867
amanavi75 <amanavi75@gmail.com>
2024-03-29 15:05:58 +0530

    started with the server starter file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-//all server related file's.   gugcjhdd
+//all server related file's. 
+
+
+const express = require("express")
+const app = express();
+const userRoutes = require("./routes/User")
+const courseRoutes = require("./routes/Course")
+const paymentRoutes = require("./routes/Payment")
+const profileRoutes = require("./routes/Profile")
